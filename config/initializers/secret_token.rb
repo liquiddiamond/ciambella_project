f10ae49d25dba4c510c39f224f847f0e62c0726b
ifthenelse <ifthenelse@ifthenelse.(none)>
2011-11-18 18:34:48 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ciambella::Application.config.secret_token = 'b382995c98376cd1acbd6c1b9ab6b7a769ff17ff7fdc24e7e3adad9a23920770693320641ff17a962c28fca1810ccfa0818656af4d40d0346a371269604094c0'
